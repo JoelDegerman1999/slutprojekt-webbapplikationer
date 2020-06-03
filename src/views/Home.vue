fd82358c7fc5008f9a16a09451cb11f8b81d3a87
Joel <joel.degerman1999@gmail.com>
2020-06-03 14:01:11 +0200

    m

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,79 @@
 <template>
-  <div></div>
+  <div class="container">
+    <section class="background" />
+    <section class="product-container">
+      <div v-if="loading">loadig....</div>
+      <article class="product" v-for="product in products" :key="product._id">
+        <div class="product-img">
+          <img :src="require(`@/assets/${product.imgFile}`)" />
+        </div>
+        <div class="product-info">
+          <h2>{{ product.title }}</h2>
+          <h3>{{ product.price }} kr</h3>
+        </div>
+      </article>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapState } from "vuex";
+export default {
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  computed: {
+    ...mapState(["products"]),
+  },
+  methods: { ...mapActions(["getAllProducts"]) },
+  async mounted() {
+    this.loading = true;
+    await this.getAllProducts();
+    this.loading = false;
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h2,
+h3 {
+  font-weight: 500;
+}
+.container {
+  height: 100%;
+  .background {
+    height: 60%;
+    background-image: url("../assets/skate-hero-1.jpg");
+    background-size: cover;
+    background-position: center;
+  }
+  .product-container {
+    padding: 2rem 6rem;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 350px;
+    gap: 5rem;
+    .product {
+      width: 100%;
+      .product-img {
+        background-color: #c4c4c4;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        height: 75%;
+        img {
+          width: 85%;
+        }
+      }
+      .product-info {
+        margin-top: 1rem;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+      }
+    }
+  }
+}
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,9 +1,47 @@
 <template>
-  <div><h1>This is a product page</h1></div>
+  <div class="container">
+    <section class="product">
+      <img src="@/assets/hoodie-ocean.png" class="product-img" />
+      <div class="product-info">
+        <h1>PRODUCT TITLE</h1>
+        <h2>599 kr</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe
+          corrupti quasi itaque numquam voluptas exercitationem corporis harum
+          eaque incidunt repellat!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          error maxime deleniti excepturi vero expedita ad, autem dolor! Ab,
+          enim?
+        </p>
+        <button>ADD TO CART</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  padding: 4rem;
+
+  .product {
+    display: flex;
+    justify-content: center;
+    .product-img {
+      width: 30%;
+    }
+    .product-info {
+      margin-top: 5rem;
+      margin-left: 2rem;
+      text-align: left;
+      width: 40rem;
+    }
+  }
+}
+</style>
