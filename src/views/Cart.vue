9163fa784990e3f34578787a5182b04920817868
sonde <sondell.mikael@gmail.com>
2020-06-08 11:12:24 +0200

    updated with headers for cart grid

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
     <div class="cart-container">
+      <div class="cart-menu">
+        <div class="menu-product">
+          <p>Produkt</p>
+        </div>
+        <div class="menu-price">
+          <p>Pris</p>
+        </div>
+        <div class="menu-amount">
+          <p>Antal</p>
+        </div>
+        <div>
+          <p class="menu-total">Total</p>
+        </div>
+      </div>
       <article class="cart" v-for="item in getCart" :key="item.product._id">
         <div class="cart-first-two-columns">
           <div class="cart-image" v-if="item.product.imgFile">
@@ -10,23 +24,19 @@
             <p>{{ item.product.title }}</p>
           </div>
         </div>
-
+        <div class="product-price">{{item.product.price}} kr</div>
         <div class="cart-quantity">
-          <img
-            src="@/assets/remove-circle-outline.svg"
-            @click="decreaseCount(item)"
-          />
+          <img src="@/assets/remove-circle-outline.svg" @click="decreaseCount(item)" />
           <p>{{ item.quantity }} st</p>
-          <img
-            src="@/assets/add-circle-outline.svg"
-            @click="increaseCount(item)"
-          />
+          <img src="@/assets/add-circle-outline.svg" @click="increaseCount(item)" />
         </div>
-        <div class="cart-price">
+        <div class="row-total">
           <p>{{ getRowTotal(item) }} kr</p>
         </div>
       </article>
-      <h1>Total {{ getTotal }}</h1>
+      <div class="cart-total">
+        <p>Total {{ getTotal }} kr</p>
+      </div>
     </div>
     <div class="nav-btn">
       <router-link to="/">
@@ -61,7 +71,7 @@ export default {
         sum += item.product.price;
       }
       return sum;
-    },
+    }
   },
   computed: {
     getCart() {
@@ -70,7 +80,7 @@ export default {
     getTotal() {
       let cart = this.getCart;
       let sum = 0;
-      cart.forEach((element) => {
+      cart.forEach(element => {
         if (element.quantity > 1) {
           for (let i = 0; i < element.quantity; i++) {
             sum += element.product.price;
@@ -80,8 +90,8 @@ export default {
         }
       });
       return sum;
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -121,6 +131,33 @@ export default {
     height: 70%;
     background: white;
 
+    .cart-menu {
+      display: flex;
+      justify-content: space-between;
+      box-sizing: border-box;
+       align-items: center;
+      padding: 3rem;
+      width: 100%;
+      height: 100%;
+
+      .menu-product {
+     
+      min-width: 25%;
+      }
+      .menu-total{
+        padding-right: 1rem;
+        min-width: 10%;
+      }
+ .menu-price{
+display:flex;
+
+ }
+      
+      p {
+        font-weight: 600;
+      }
+    }
+
     article.cart {
       display: flex;
       justify-content: space-between;
@@ -132,19 +169,28 @@ export default {
 
       .cart-first-two-columns {
         display: flex;
+        min-width: 30%;
 
         .cart-image {
           img {
             height: 2rem;
+            align-items: center;
           }
         }
         .cart-title {
           padding-left: 2rem;
         }
       }
+      .product-price {
+      min-width: 10%;
+      }
 
+      .row-total{
+        width: 10%;
+      }
       .cart-quantity {
         display: flex;
+        min-width: 6%;
 
         img {
           padding: 0.1rem;
@@ -154,9 +200,18 @@ export default {
           color: black;
         }
       }
-      // .cart-price {
-
-      // }
+    }
+    .cart-total {
+      width: 100%;
+      border-top: 1px solid rgba(0, 0, 0, 0.3);
+      display: flex;
+      justify-content: space-between;
+      box-sizing: border-box;
+      justify-content: flex-end;
+      padding: 3rem;
+      p {
+        font-weight: 600;
+      }
     }
   }
 }
