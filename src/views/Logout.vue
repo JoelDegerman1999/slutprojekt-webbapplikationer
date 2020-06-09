cccd4fc86743e45df893cb72da5811d1ad7dc122
Joel Degerman <joel@joeldegerman.se>
2020-06-09 10:20:28 +0200

    Added logout page and added admin btn in profile

---
+++ src/views/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>You are logged out</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+      this.$router.push("/");
+    }, 2000);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 2rem;
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,120 +1,121 @@
 <template>
-<section>
-  <div id="profileContainer">
-    <div id="profilePicture"></div>
-    <h1>{{currentUser.name}}</h1>
-    <h2>{{currentUser.email}}</h2>
-    <div class="adressInformation">
-      <h1 class="shippingAdress">SHIPPING ADRESS</h1>
-      <h3>{{currentUser.adress.street}}</h3>
-      <h3>{{currentUser.adress.zip}}</h3>
-      <h3>{{currentUser.adress.city}}</h3>
-    </div>
-    
-    <h1>ORDER HISTORY</h1>
-    <div class="orderHistory" v-if= "orders != undefined">
-      <div v-for= "(order, index) in orders"
-      v-bind:key= "order._id"
-      class="order">
-        <div class="orderFormat">
+  <section>
+    <div id="profileContainer">
+      <div id="profilePicture"></div>
+      <h1>{{currentUser.name}}</h1>
+      <h2>{{currentUser.email}}</h2>
+      <button class="admin" v-if="currentUser.role == 'admin'">Admin page</button>
+      <div class="adressInformation">
+        <h1 class="shippingAdress">SHIPPING ADRESS</h1>
+        <h3>{{currentUser.adress.street}}</h3>
+        <h3>{{currentUser.adress.zip}}</h3>
+        <h3>{{currentUser.adress.city}}</h3>
+      </div>
+
+      <h1>ORDER HISTORY</h1>
+      <div class="orderHistory" v-if="orders != undefined">
+        <div v-for="(order, index) in orders" v-bind:key="order._id" class="order">
+          <div class="orderFormat">
             <h4>ID: {{order._id}}</h4>
             <h4>{{new Date(order.timeStamp).toISOString().substring(0, 10)}}</h4>
-        </div>
-        
-          <div v-for= "orderItem in computedOrders[index]"
-          v-bind:key= "orderItem._id">
+          </div>
+
+          <div v-for="orderItem in computedOrders[index]" v-bind:key="orderItem._id">
             <div class="orderFormat">
               <p>{{orderItem.title}}</p>
               <p>{{orderItem.price}} kr</p>
             </div>
-          
           </div>
-        <div class="orderFormat">
-          <h3>TOTAL:</h3>
-          <h3>{{order.orderValue}} kr</h3>
+          <div class="orderFormat">
+            <h3>TOTAL:</h3>
+            <h3>{{order.orderValue}} kr</h3>
+          </div>
         </div>
-        
       </div>
+      <div v-else>You have no placed orders.</div>
+      <button v-on:click="addFakeOrderHistory">ADD ORDER</button>
     </div>
-    <div v-else>You have no placed orders.</div>
-    <button v-on:click= "addFakeOrderHistory">ADD ORDER</button>
-  </div>
   </section>
 </template>
 
 <script>
 export default {
-
-  data:() => ({
+  data: () => ({
     orders: [],
-    computedOrders: [],
+    computedOrders: []
   }),
 
-
-  methods:{
-    addFakeOrderHistory(){
+  methods: {
+    addFakeOrderHistory() {
       let fakeOrder = {
-        _id:666,
+        _id: 666,
         timeStamp: Date.now(),
-        status: 'inProcess',
-        items:['EDSfeUr2tYY89Ajv'],
+        status: "inProcess",
+        items: ["EDSfeUr2tYY89Ajv"],
         orderValue: 999
-      }
+      };
 
       let fakeOrder2 = {
-        _id:999,
+        _id: 999,
         timeStamp: Date.now(),
-        status: 'inProcess',
-        items:['ypgZaZRXYjnQOhQI', 'rQRnERmOBjyhKufi'],
+        status: "inProcess",
+        items: ["ypgZaZRXYjnQOhQI", "rQRnERmOBjyhKufi"],
         orderValue: 899
-      }
+      };
 
       this.$store.state.user.orderHistory = [];
       this.$store.state.user.orderHistory.push(fakeOrder);
       this.$store.state.user.orderHistory.push(fakeOrder2);
     },
 
-    async getCurrentItem(productId){
-      let prod = await this.$store.dispatch('readProduct', productId);
+    async getCurrentItem(productId) {
+      let prod = await this.$store.dispatch("readProduct", productId);
       return prod;
     }
   },
-  computed:{
-    currentUser(){
+  computed: {
+    currentUser() {
       //Hämta från localStorage istället?
       return this.$store.state.user;
-    },
+    }
   },
 
-  async mounted(){
-      let currentUser = this.$store.state.user;
-      if(Object.keys(currentUser).length === 0){
-        this.$router.push("/login/account");
-      }
+  async mounted() {
+    let currentUser = this.$store.state.user;
+    if (Object.keys(currentUser).length === 0) {
+      this.$router.push("/login/account");
+    }
 
-      this.orders = currentUser.orderHistory;
-      //undefined == användaren har inga ordrar
-      if(this.orders != undefined){
-        for(let i = 0; i < this.orders.length; i++){
-          this.computedOrders.push([]);
-            for(let j = 0; j < this.orders[i].items.length; j++){
-              this.computedOrders[i].push(await this.$store.dispatch('readProduct', this.orders[i].items[j]));
-            }
+    this.orders = currentUser.orderHistory;
+    //undefined == användaren har inga ordrar
+    if (this.orders != undefined) {
+      for (let i = 0; i < this.orders.length; i++) {
+        this.computedOrders.push([]);
+        for (let j = 0; j < this.orders[i].items.length; j++) {
+          this.computedOrders[i].push(
+            await this.$store.dispatch("readProduct", this.orders[i].items[j])
+          );
         }
       }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-#profileContainer{
+.admin {
+  margin-top: 0;
+  margin-bottom: 2rem;
+}
+
+#profileContainer {
   margin-top: 3rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  #profilePicture{
+  #profilePicture {
     width: 15rem;
     height: 15rem;
     border-radius: 30rem;
@@ -123,33 +124,32 @@ export default {
     margin-bottom: 2rem;
   }
 
-  .shippingAdress{
+  .shippingAdress {
     font-size: 2rem;
   }
 
-  .adressInformation{
+  .adressInformation {
     margin-bottom: 2rem;
   }
 
-  .orderFormat{
+  .orderFormat {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
-  
-  .order{
+  .order {
     border-bottom: 5px solid black;
     width: 20rem;
     margin-bottom: 1rem;
   }
 
-  h1{
+  h1 {
     font-size: 3rem;
     margin-bottom: 1rem;
   }
 
-  h2{
+  h2 {
     margin-bottom: 2rem;
   }
 }
